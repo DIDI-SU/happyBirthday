3a1cf9da9ec0e07c0d0f3e07f0b2f2ac79f81f87
DIDI-SU <jisoodidi86@gmail.com>
2025-03-08 18:05:04 +0900

    [Fix]날짜 수정

--- src/page/comingsoon/index.tsx
+++ src/page/comingsoon/index.tsx
@@ -11,7 +11,7 @@ const ComingSoon = () => {
         />
         <div className="flex flex-col items-center justify-center">
           <h1 className="text-white text-xl sm:text-2xl md:text-3xl lg:text-4xl font-normal font-abordage">
-            3/19, 2025
+            3/21, 2025
           </h1>
           <h2 className="text-white text-base sm:text-xl md:text-2xl lg:text-2xl font-normal">
             Coming soon
