a242dc9c33e25ea3760bd68cb21059e256579962
DIDI-SU <jisoodidi86@gmail.com>
2025-03-08 19:04:42 +0900

    [ADD] 효과 및 사운드 추가

--- src/page/comingsoon/index.tsx
+++ src/page/comingsoon/index.tsx
@@ -1,22 +1,48 @@
+import { useState } from "react";
 import { ComingSoonImage } from "../memo/icon/MemoIcon";
-
+import { useTranslation } from "react-i18next";
+import useSound from "use-sound";
 const ComingSoon = () => {
+  const { t } = useTranslation();
+  const [isAnimating, setIsAnimating] = useState(false);
+  const [play] = useSound("/sounds/coming3.mp3", {
+    sprite: {
+      short: [0, 500], // 0ms부터 1000ms(1초)까지만 재생
+    },
+  });
+
+  const handlePlay = async () => {
+    try {
+      play({ id: "short" }); // 단순히 play() 호출
+      setIsAnimating(true);
+      // 애니메이션 종료 후 상태 리셋
+      setTimeout(() => setIsAnimating(false), 800); // 0.8초 후 리셋
+    } catch (error) {
+      console.error("오디오 재생 실패:", error);
+    }
+  };
+
   return (
     <div className="w-full h-[100dvh] flex flex-col items-center justify-center bg-black">
-      <div className="w-full max-w-[1200px] px-4 md:px-6 lg:px-8">
+      <div
+        className={`w-full max-w-[1200px] px-4 mb-6  md:px-6 lg:px-8 cursor-pointer ${
+          isAnimating ? "wobble-hor-bottom" : ""
+        }`}
+        onClick={handlePlay}
+      >
         <ComingSoonImage
           width="100%"
           height="auto"
-          className="w-full md:w-[500px] lg:w-[665px] mx-auto"
+          className="w-[300px] md:w-[500px] lg:w-[665px] mx-auto"
         />
-        <div className="flex flex-col items-center justify-center">
-          <h1 className="text-white text-xl sm:text-2xl md:text-3xl lg:text-4xl font-normal font-abordage">
-            3/22, 2025
-          </h1>
-          <h2 className="text-white text-base sm:text-xl md:text-2xl lg:text-2xl font-normal">
-            Coming soon
-          </h2>
-        </div>
+      </div>
+      <div className="flex flex-col items-center justify-center">
+        <h1 className="text-white mb-2 text-2xl sm:text-2xl md:text-3xl lg:text-4xl font-normal font-abordage">
+          {t("comingsoon.date")}
+        </h1>
+        <h2 className="text-white mt-3 text-base sm:text-xl md:text-2xl lg:text-2xl font-normal font-abordage">
+          {t("comingsoon.title")}
+        </h2>
       </div>
     </div>
   );
