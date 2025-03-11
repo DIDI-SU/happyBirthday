9f6ce997ea2bcc91836b05cdb1a5ed178779f79e
DIDI-SU <jisoodidi86@gmail.com>
2025-03-11 23:05:23 +0900

    [add] dev 용

--- src/router/routeritems.tsx
+++ src/router/routeritems.tsx
@@ -1,22 +1,22 @@
 import ComingSoon from "../page/comingsoon";
 import Error404 from "../page/error/404";
 import Main from "../page/main/index";
-import Memo from "../page/memo/Memo";
-import Write from "../page/write/Write";
+// import Memo from "../page/memo/Memo";
+// import Write from "../page/write/Write";
 
 const routerItems = [
   {
     path: "/",
+    element: <ComingSoon />,
+  },
+  {
+    path: "/main",
     element: <Main />,
   },
-  {
-    path: "/memo",
-    element: <Memo />,
-  },
-  {
-    path: "/write/:memo",
-    element: <Write />,
-  },
+  // {
+  //   path: "/write/:memo",
+  //   element: <Write />,
+  // },
   {
     path: "*",
     element: <Error404 />,
