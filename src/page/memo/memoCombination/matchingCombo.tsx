95ef2b19eb324f43e03786ae3fa10116ddbd37c3
DIDI-SU <jisoodidi86@gmail.com>
2025-03-19 20:44:51 +0900

    [Add] 저장 버튼 추가

--- src/page/memo/memoCombination/matchingCombo.tsx
+++ src/page/memo/memoCombination/matchingCombo.tsx
@@ -13,7 +13,7 @@ import {
 import { MemoItem } from "../../selectIcon/type/selectIcon";
 import { useTranslation } from "react-i18next";
 
-import { useContext, useState } from "react";
+import { useContext, useMemo, useState } from "react";
 import { FCMContext } from "../../../context/FCMContext";
 import { setDoc, doc } from "firebase/firestore";
 import { v4 as uuidv4 } from "uuid";
@@ -45,6 +45,7 @@ const MatchingCombo = ({ matchingCombo }: MatchingComboProps) => {
   const [searchParams] = useSearchParams();
   const { database } = useContext(FCMContext);
   const [message, setMessage] = useState({ id: 0, messages: "" });
+  const [isSaving, setIsSaving] = useState(false);
   const navigate = useNavigate();
 
   const postMemo = async () => {
@@ -57,31 +58,17 @@ const MatchingCombo = ({ matchingCombo }: MatchingComboProps) => {
         createdAt: new Date(),
       });
       setMessage({ id: 0, messages: "" });
+      setIsSaving(true);
       navigate("/achive");
     } catch (error) {
       console.error("Setup error:", error);
+      setIsSaving(false);
     }
   };
 
-  //   useEffect(() => {
-  //     try {
-  //       const memoRef = collection(database, "message");
-
-  //       const unsubscribe = onSnapshot(memoRef, (snapshot) => {
-  //         const data = snapshot.docs.map((doc) => doc.data());
-  //         setMemoData(data);
-  //         console.log("Fetched data:", data);
-  //       });
-
-  //       return () => unsubscribe();
-  //     } catch (error) {
-  //       console.error("Setup error:", error);
-  //     }
-  //   }, [database]);
-
   const idArray = searchParams.getAll("id").map(Number); // id 파라미터의 모든 값을 배열로 가져옵니다
 
-  const findMatchingCombination = () => {
+  const findMatchingCombination = useMemo(() => {
     const result = matchingCombo.find((combo) => {
       return combo.pairs.some((pair) => {
         return (
@@ -104,37 +91,39 @@ const MatchingCombo = ({ matchingCombo }: MatchingComboProps) => {
       };
     }
     return result;
-  };
+  }, [idArray, matchingCombo]);
   //
-  const result = findMatchingCombination();
 
   return (
     <section className="flex flex-col items-center justify-center w-full px-[16px] absolute">
       <div className="flex flex-col items-center justify-center ">
         <p className=" text-white text-nowrap text-[15px] ">
-          {t(`confetti.characters.${result?.name}.subtitle`)}
+          {t(`confetti.characters.${findMatchingCombination?.name}.subtitle`)}
         </p>
         <p className=" text-white text-nowrap text-[25px] ">
-          {t(`confetti.characters.${result?.name}.title`)}
+          {t(`confetti.characters.${findMatchingCombination?.name}.title`)}
         </p>
       </div>
       <div className="flex flex-col items-center justify-center max-h-[50dvh] w-full mx-auto ">
         <svg viewBox="0 0 200 200">
-          {MEMO_LIST.find((item) => item.id === result?.id)?.svg}
+          {
+            MEMO_LIST.find((item) => item.id === findMatchingCombination?.id)
+              ?.svg
+          }
           <foreignObject
             x="70%"
             y="72%"
             width={170}
-            height={170}
+            height={160}
             transform="translate(-125,-125)"
             className="p-6 "
           >
             <textarea
-              className=" w-full h-full bg-transparent border-dotted border-2 border-gray-600 rounded-lg p-2 resize-none  text-gray-600 text-opacity-80 text-[12px]"
+              className=" w-full h-full bg-transparent border-dotted border-2 border-gray-600 rounded-lg px-2 pt-2 resize-none  text-gray-600 text-opacity-80 text-[12px]"
               placeholder="호영을 위한 메세지를 작성해주세요"
               onChange={(e) => {
                 const memoId = MEMO_LIST.find(
-                  (item) => item.id === result?.id
+                  (item) => item.id === findMatchingCombination?.id
                 )?.id;
 
                 if (memoId !== undefined) {
@@ -142,29 +131,51 @@ const MatchingCombo = ({ matchingCombo }: MatchingComboProps) => {
                 }
               }}
             />
-            <button onClick={postMemo} className="bg-blue-500 rounded-md">
-              저장
-            </button>
+            <div className="flex flex-row items-center justify-end w-full absolute bottom-3 right-4 ">
+              <button
+                onClick={postMemo}
+                className={`text-sm w-[30%] -rotate-6 cursor-pointer  ${
+                  isSaving ? "bg-gray-500" : "bg-blue-500"
+                } rounded-md`}
+                disabled={isSaving}
+              >
+                {isSaving ? "저장중..." : "보내기!"}
+              </button>
+            </div>
           </foreignObject>
         </svg>
       </div>
       <div className="flex flex-col items-center justify-center w-full  pt-3 ">
         <p className=" text-white  text-nowrap text-[18px] leading-6 ">
-          {t(`confetti.characters.${result?.name}.description1`)}
+          {t(
+            `confetti.characters.${findMatchingCombination?.name}.description1`
+          )}
         </p>
-        {t(`confetti.characters.${result?.name}.description2`) && (
+        {t(
+          `confetti.characters.${findMatchingCombination?.name}.description2`
+        ) && (
           <p className=" text-nowrap  text-white text-[18px] leading-6">
-            {t(`confetti.characters.${result?.name}.description2`)}
+            {t(
+              `confetti.characters.${findMatchingCombination?.name}.description2`
+            )}
           </p>
         )}
-        {t(`confetti.characters.${result?.name}.description3`) && (
+        {t(
+          `confetti.characters.${findMatchingCombination?.name}.description3`
+        ) && (
           <p className="text-nowrap  text-white  text-[18px] leading-6">
-            {t(`confetti.characters.${result?.name}.description3`)}
+            {t(
+              `confetti.characters.${findMatchingCombination?.name}.description3`
+            )}
           </p>
         )}
-        {t(`confetti.characters.${result?.name}.description4`) && (
+        {t(
+          `confetti.characters.${findMatchingCombination?.name}.description4`
+        ) && (
           <p className=" text-nowrap  text-white text-[18px] leading-6">
-            {t(`confetti.characters.${result?.name}.description4`)}
+            {t(
+              `confetti.characters.${findMatchingCombination?.name}.description4`
+            )}
           </p>
         )}
       </div>
